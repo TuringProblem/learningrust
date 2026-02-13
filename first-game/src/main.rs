7328169ca7fbd0150a0db1b96e4d4266354dbc89
Override <122590935+TuringProblem@users.noreply.github.com>
2026-02-12 22:55:36 -0500

    feat: okay we getting a good feel for it

--- first-game/src/main.rs
+++ first-game/src/main.rs
@@ -1,3 +1,5 @@
+use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
 
 /**
@@ -7,12 +9,49 @@ use std::io;
 fn main() {
     println!("Guess the number!");
 
-    println!("Please enter a number: ");
+    let random_number: u32 = rand::thread_rng().gen_range(1..=100);
+    let mut is_over: bool = true;
 
-    let mut input = String::with_capacity(10);
+    while is_over {
+        let input: String = retrieve_input_data();
+
+        let input: u32 = input
+            .trim()
+            .parse()
+            .expect("Please enter a number between 1 and 100");
+
+        match input.cmp(&random_number) {
+            Ordering::Less => println!("You guessed: {input} and the number was Less than"),
+            Ordering::Greater => {
+                println!("You guessed: {random_number} and the secret number was greater")
+            }
+            Ordering::Equal => {
+                println!("YOU WON!");
+                is_over = false
+            }
+        }
+
+        println!("Continue? y = yes, n = no");
+        let mut continue_line: String = String::with_capacity(1);
+        io::stdin().read_line(&mut continue_line).expect("Failed to read line");
+
+        match continue_line.trim() {
+            "y" => continue,
+            "n" => {
+                is_over = false; 
+                println!("Have a good day :)")
+            }
+            _ => println!("alright, we are gonna go again then lol...")
+        }
+    }
+}
+
+fn retrieve_input_data() -> String {
+    println!("Please enter a number between 1 and 100: ");
+    let mut input: String = String::with_capacity(10);
     io::stdin()
         .read_line(&mut input)
         .expect("Failed to read line");
 
-    println!("You guessed: {input}");
+    input
 }
